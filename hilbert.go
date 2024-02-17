d0df97ab33789f30b1a5dff589e3311cd6d8d82f
Inotart <121980156@qq.com>
2024-02-17 15:40:34 +0800

    吐槽.mp4

---
+++ hilbert.go
@@ -0,0 +1,15 @@
+package hilbert
+
+/*
+	将多维数组data 转换为单维具有顺序的数组
+	参数 length 的数量越多,维度越高,至少需要2个维度才能开始计算
+*/
+func New_Hilbert_map(length ...int) ([]int, error) {
+
+	return nil, nil
+}
+
+type Hilbert_map struct {
+	Length    int
+	Dimension int
+}
---
+++ 测试/demo.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Inotart/hilbert"
+
+func main() {
+	hilbert.New_Hilbert_map()
+}
